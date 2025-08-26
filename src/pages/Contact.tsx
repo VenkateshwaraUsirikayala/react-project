5210e7abfb7e58359bfa5258b9061a0df2e05bad
venkateshwarausirikayala <usirikayalav@gmail.com>
2025-08-27 00:09:01 +0530

    send message dialog box

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -30,7 +30,7 @@ const Contact = () => {
     setIsLoading(true);
 
     try {
-    const response = await fetch('https://appplicationloadbalancer-74626486.us-east-1.elb.amazonaws.com/contact-submit', {
+    const response = await fetch('https://backend.bhavanjali.art/contact-submit', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
